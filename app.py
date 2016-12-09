2e426020246d4a4dd62ed9bbe04f730b1fbdca67
Michele Sorcinelli <michelesr@autistici.org>
2016-12-09 15:59:11 +0100

    Provide a better example app

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
-from flask import Flask
+import json
+from flask import Flask, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+db = MongoClient('db')
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/api/students/', methods=['POST', 'GET'])
+def student():
+    if request.method == 'POST':
+        # get the json
+        student = request.get_json(force=True)
+
+        # check data
+        if not 'name' in student.keys():
+            return json.dumps({
+                'status': 'error: student must have a name'
+            }), 400
+        elif not 'grades' in student.keys():
+            return json.dumps({
+                'status': 'error: student must have grades'
+            }), 400
+
+        # insert in db
+        students = db.app.students
+        students.insert_one(student)
+
+        return json.dumps({'status': 'student inserted'})
+    else:
+        # return the students in db
+        students = []
+        for s in db.app.students.find():
+            students.append({'name': s['name'], 'grades': s['grades']})
+        return json.dumps(students)
+
 
 app.run(host='0.0.0.0', port=8000)
